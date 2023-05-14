ace49ecba5e1c75b002bbe10df1b7362982ac0d5
Cuong Nguyen <cuong@taureau.ai>
2023-05-14 11:43:22 +0700

    [BE] Implement User and Scope table

---
+++ monailabel/endpoints/user/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from database import Base
+
+class Scope(Base):
+    __tablename__ = 'scope'
+    id = Column(Integer, primary_key=True, index=True)
+    scope = Column(String, unique=True, index=True)
+    users = relationship("User", back_populates="scope")
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    full_name = Column(String, index=True)
+    disabled = Column(Boolean, default=True)
+    scopes = relationship("User", back_populates="user")
