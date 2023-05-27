0d70bf74c2c19c900f46b948bd15647bbe53df31
Cuong Nguyen <cuong@taureau.ai>
2023-05-27 15:32:37 +0700

    [BE] update scopes field

--- monailabel/endpoints/user/models.py
+++ monailabel/endpoints/user/models.py
@@ -1,14 +1,26 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, types
 from sqlalchemy.orm import relationship
 from monailabel.database import Base
 
-# class Scope(Base):
-#     __tablename__ = 'scope'
-#     id = Column(Integer, primary_key=True, index=True)
-#     scope = Column(String, unique=True, index=True)
-#     users = relationship("User", back_populates="scope")
+class ChoiceType(types.TypeDecorator):
+    impl = types.String
+
+    def __init__(self, choices, **kw):
+        self.choices = dict(choices)
+        super(ChoiceType, self).__init__(**kw)
+
+    def process_bind_param(self, value, dialect):
+        return [k for k, v in self.choices.items() if v == value][0]
+
+    def process_result_value(self, value, dialect):
+        return self.choices[value]
 
 class User(Base):
+    SCOPES = {
+        "admin": "admin",
+        "user": "user"
+    }
+
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
@@ -16,4 +28,4 @@ class User(Base):
     hashed_password = Column(String)
     full_name = Column(String, index=True)
     disabled = Column(Boolean, default=True)
-    # scopes = relationship("Scope", back_populates="user")
+    scopes = Column(ChoiceType(SCOPES), default='user')
